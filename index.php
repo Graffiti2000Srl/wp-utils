2ee4231bc70a989e0fad32e9885c5c55074e7d70
Zweer <flicofloc@gmail.com>
2014-03-21 16:38:06 +0100

    Added the ability to remove comments

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 class G2K_WP_Utils
 {
-    public $removeAdminBar = true;
+    public $removeAdminBar = false;
+    public $removeComments = false;
 
     public function __construct()
     {
@@ -19,6 +20,29 @@ class G2K_WP_Utils
     {
         if ($this->removeAdminBar) {
             add_filter('show_admin_bar', '__return_false');
+        }
+
+        if ($this->removeComments) {
+            # Removes comments from admin menu
+            add_action('admin_menu', 'my_remove_admin_menus');
+            function my_remove_admin_menus () {
+                remove_menu_page('edit-comments.php');
+            }
+
+            # Removes comments from post and pages
+            add_action('init', 'remove_comment_support', 100);
+            function remove_comment_support() {
+                remove_post_type_support('post', 'comments');
+                remove_post_type_support('page', 'comments');
+            }
+
+            # Removes comments from admin bar
+            add_action('wp_before_admin_bar_render', 'mytheme_admin_bar_render');
+            function mytheme_admin_bar_render() {
+                global $wp_admin_bar;
+
+                $wp_admin_bar->remove_menu('comments');
+            }
         }
     }
 } 
