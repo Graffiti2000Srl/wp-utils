e93420013ba3370cd3803d1d9b0be94c32d96a56
Zweer <flicofloc@gmail.com>
2014-03-21 16:35:13 +0100

    Added the possibility to hide the admin bar

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 
 class G2K_WP_Utils
 {
+    public $showAdminBar = true;
+
     public function __construct()
     {
         define('WP_THEME_PATH', get_template_directory());
         define('WP_THEME_URI', get_template_directory_uri());
+    }
+
+    public function init()
+    {
+        $this->_showHideThings();
+    }
+
+    protected function _showHideThings()
+    {
+        if (!$this->showAdminBar) {
+            add_filter('show_admin_bar', '__return_false');
+        }
     }
 } 
